52e1fd63c770ff89299844cfb9c82afa7a591b9a
Jettchen <jettchen12345@gmail.com>
2019-10-26 11:05:37 +0800

    Add a python styled quicksort, and a quicksort version without recursion

--- HighA/Algorithm/quicksort.py
+++ HighA/Algorithm/quicksort.py
@@ -1,7 +1,7 @@
 # mergesort
 from random import randint
 lis = []
-for i in range(5000):
+for i in range(100):
 	lis.append(randint(0,100))
 
 def quick_one(lis):
@@ -29,6 +29,29 @@ def quick_one(lis):
 			pMin+=1
 	return nList,keyI
 
+def quick_onePy(lis):
+	"""python styled quick one"""
+	key = lis[0]
+	smLis = []
+	bgLis = []
+	for n in lis[1:]:
+		if n<=key:
+			smLis.append(n)
+		else:
+			bgLis.append(n)
+	return smLis+[key]+bgLis,len(smLis)
+
+def quick_sortPy(lis):
+	"""
+		quick sort with recursion, python styled
+	"""
+	nLis = lis.copy()
+	if len(lis) <= 1:
+		return lis
+	else:
+		nLis,keyI = quick_onePy(nLis)
+		return quick_sort(nLis[:keyI])+[nLis[keyI]]+quick_sort(nLis[keyI+1:])
+
 def quick_sort(lis):
 	"""
 		quick sort with recursion
@@ -40,6 +63,49 @@ def quick_sort(lis):
 		nLis,keyI = quick_one(nLis)
 		return quick_sort(nLis[:keyI])+[nLis[keyI]]+quick_sort(nLis[keyI+1:])
 
-print(lis)
-nlis = quick_sort(lis)
+def quick_oneNR(lis,pMin,keyI,pMax):
+	nList = lis.copy()
+	key  = lis[keyI]
+	while(pMin!=pMax):
+		while pMax>keyI:
+			if nList[pMax]<=key:
+				nList[pMax],nList[keyI] = nList[keyI],nList[pMax]
+				keyI = pMax
+				break
+			pMax-=1
+		while pMin<keyI:
+			if nList[pMin]>key:
+				nList[pMin],nList[keyI] = nList[keyI],nList[pMin]
+				keyI = pMin
+				break
+			pMin+=1
+	return nList,keyI
+
+def quick_sortNR(lis):
+	"""quick sort without recursion"""
+	mem = [(0,0,len(lis)-1)]
+	c = 1
+	while len(mem)!=0:
+		p = 0
+		# print(c)
+		c+=1
+		# print(mem[p])
+		lis,keyI = quick_oneNR(lis,mem[p][0],mem[p][0],mem[p][2])
+		if len(lis) == 0:
+			# print("break")
+			break
+		else:
+			if (keyI-1)-mem[p][0]>0:
+				mem.append((mem[p][0],mem[p][0],keyI-1))
+			if (mem[p][-1])-(keyI+1)>0:
+				mem.append((keyI+1,keyI+1,mem[p][-1]))
+		mem.pop(p)
+		# print("ed")
+	# print("out of while loop")
+	return lis
+# print(lis)
+nlis = quick_sortNR(lis)
+print("done")
 print(nlis)
+if nlis==sorted(lis):
+	print("YAY!")
